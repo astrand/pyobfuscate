de57b56d434ee588bf3f1d8e40560ed93e057ee3
astrand <>
2004-05-28 12:38:22 +0000

    Simplified Windows cmd wrapper; start is not necessary.

--- pyobfuscate.cmd
+++ pyobfuscate.cmd
@@ -4,4 +4,4 @@ rem Windows wrapper for Python scripts. Allows you to invoke a Python
 rem script as "myscript" instead of "myscript.py". 
 rem Author: Peter Astrand <peter@cendio.com>
 rem
-start /b /wait %~dpn0.py %*
+%~dpn0.py %*
