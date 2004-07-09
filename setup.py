6e48883ebe6fd9257a79410ba4048197dcc76fbf
astrand <>
2004-07-09 11:25:42 +0000

    Bumped up version to 0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Python source code obfuscator
 from distutils.core import setup
 
 setup (name = "pyobfuscate",
-       version = "0.1",
+       version = "0.2",
        license = "GPL",
        description = "pyobfuscate",
        long_description = DESCRIPTION,
