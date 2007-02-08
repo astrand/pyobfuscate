459063bc319aa71737cf6473e9a7dc3992325a55
astrand <>
2007-02-08 12:13:25 +0000

    Updated email and copyright

--- pyobfuscate.cmd
+++ pyobfuscate.cmd
@@ -2,6 +2,6 @@
 rem
 rem Windows wrapper for Python scripts. Allows you to invoke a Python
 rem script as "myscript" instead of "myscript.py". 
-rem Author: Peter Astrand <peter@cendio.com>
+rem Author: Peter Astrand <astrand@cendio.com>
 rem
 %~dpn0.py %*
--- pyobfuscate.py
+++ pyobfuscate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Execute the file with the same name as myself minus the extension.
-# Author: Peter Astrand <peter@cendio.com>
+# Author: Peter Astrand <astrand@cendio.com>
 #
 import os, sys
 root, ext = os.path.splitext(sys.argv[0])
