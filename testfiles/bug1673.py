e0a35af287ee9d71a67b7f370d2d9b65ed2fedd6
astrand <>
2009-06-16 10:57:36 +0000

    Enhanced test case: Make sure that local variables are obfuscated, even if a global variable with the same name shouldn't be.

--- testfiles/bug1673.py
+++ testfiles/bug1673.py
@@ -1,9 +1,11 @@
-__all__ = ["gvar"]
+__all__ = ["gvar", "colliding"]
 import sys
 gvar = 47
 def foo():
     global gvar
     gvar += 2
+    colliding = 33
+    assert 0 == locals().has_key("colliding")
     return gvar
 sys.exit(foo())
 
