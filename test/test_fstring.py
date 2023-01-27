04a1237428287662839795502990b2bff5579920
Peter Astrand <peter.astrand@etteplan.com>
2023-01-27 15:22:03 +0100

    Add support for f-strings

---
+++ test/test_fstring.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+import unittest
+
+globalval = 999
+
+class C:
+    foo = 42
+    pass
+c = C()
+
+multiline = f"""
+param1={globalval}
+param2={c.foo}
+"""
+
+class FStringTest(unittest.TestCase):
+    def test_strings(self):
+        value = 17
+        self.assertEqual(f"{value*value}", "289")
+        self.assertEqual(f"{value!s}", "17")
+        self.assertEqual(f"{value:4}", "  17")
+        self.assertEqual(f"{globalval}dollars", "999dollars")
+        self.assertTrue(f"{sys.version}".startswith("3."))
+        self.assertEqual(multiline, """
+param1=999
+param2=42
+""")
+
+        self.assertEqual(f"a {value} "
+                         f"b {globalval} ", "a 17 b 999 ")
+        
+
+if "__main__" == __name__:
+    unittest.main()
